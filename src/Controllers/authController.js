bcd316e13ba33409fdabfd9cede246dcf50f45cb
“n84374161” <nayer.nasef.hanallah1@h-partners.com>
2025-02-10 16:23:13 +0200

    Is Ban Updated

--- src/Controllers/authController.js
+++ src/Controllers/authController.js
@@ -22,17 +22,23 @@ const setAuthCookies = (res, accessToken, refreshToken) => {
 };
 
 // Helper to handle errors
-const handleError = (res, error) => res.status(400).json({ error: error.message });
+const handleError = (res, error) =>
+  res.status(400).json({ error: error.message });
 
 // Login Function
 export const loginUser = asyncHandler(async (req, res) => {
   const { Email, Password } = req.body;
-  const user = await prisma.user.findUnique({ where: { Email } });
-
-  if (!user) return res.status(404).json({ message: "User not found" });
-
+  const user = await prisma.user.findUnique({
+    where: {
+      Email: Email,
+    },
+  });
+  if (!user) return res.status(404).json({ message: "Email Or Password Is Wrong" });
+  if (user.IsBan) return res.status(403).json({ message: "User is banned" });
+  
   const isPasswordValid = bcrypt.compareSync(Password, user.Password);
-  if (!isPasswordValid) return res.status(401).json({ message: "Invalid password" });
+  if (!isPasswordValid)
+    return res.status(401).json({ message: "Email Or Password Is Wrong" });
 
   // Generate tokens
   const { accessToken, refreshToken } = generateTokens(user);
@@ -43,14 +49,17 @@ export const loginUser = asyncHandler(async (req, res) => {
   // Set tokens in cookies
   setAuthCookies(res, accessToken, refreshToken);
 
-  res.status(200).json({ message: "Login successful", user: user.Email, Role: user.Role });
+  res
+    .status(200)
+    .json({ message: "Login successful", user: user.Email, Role: user.Role });
 });
 
 // Refresh Access Token
 export const refreshAccessToken = asyncHandler(async (req, res) => {
   const refreshToken = req.cookies.refreshToken;
 
-  if (!refreshToken) return res.status(401).json({ message: "Refresh Token expired" });
+  if (!refreshToken)
+    return res.status(401).json({ message: "Refresh Token expired" });
 
   const user = await prisma.user.findFirst({ where: { refreshToken } });
 
@@ -76,7 +85,8 @@ export const logoutUser = asyncHandler(async (req, res) => {
   await prisma.user.update({ where: { id }, data: { refreshToken: null } });
 
   // Clear cookies
-  res.clearCookie("accessToken")
+  res
+    .clearCookie("accessToken")
     .clearCookie("refreshToken")
     .status(200)
     .json({ message: "Logged out successfully" });
