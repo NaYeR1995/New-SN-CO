14da9b5e476fb9bb19c6d43991569a9032783507
Nayer Nassef <nayer.nasef2016@gmail.com>
2025-02-15 14:47:21 +0200

    refreshAccessToken

--- src/Utils/refreshToken.js
+++ src/Utils/refreshToken.js
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { generateTokens } from "./jwtUtils.js";
 import asyncHandler from "express-async-handler";
 import { setAuthCookies } from "../Controllers/authController.js";
+import {verifyAccessToken} from "./jwtUtils.js";
 const prisma = new PrismaClient();
 
 // Refresh Access Token
@@ -12,27 +13,18 @@ export const refreshAccessToken = asyncHandler(async (req, res, next) => {
     return res.status(404).json({ message: "You Must Login!" });
   }
 
-  const timeNow = Date.now();
-  const user = await prisma.user.findFirst({
-    where: {
-      refreshToken: refreshToken,
-      refreshTokenExpiry: {
-        lte: timeNow,
-      },
-    },
-  });
-  if (!user) return res.status(404).json({ message: "expired Refresh Token" });
+  try {
+    // Generate a new access token
+    const user = verifyAccessToken(refreshToken);
+    const { accessToken } = generateTokens(user);
+    
+    // Set the new access token in cookies
+    setAuthCookies(res, accessToken);
+    req.user = user;
+    next();
 
-  // Generate a new access token
-  const { accessToken } = generateTokens(user);
-
-  // Set the new access token in cookies
-  setAuthCookies(res, accessToken);
-  req.user = user;
-  next();
-
-  res.status(200).json({ message: "Token refreshed successfully" });
-
-  // Return the new access token
-  return accessToken;
+    res.status(200).json({ message: "Token refreshed successfully" });
+  } catch (error) {
+    return res.status(401).json({ message: `You Must Login!` });
+  }
 });
